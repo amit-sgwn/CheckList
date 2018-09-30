b2ddacf416181483b4200dde3ec10912ae6e8ade
Amit Sangwan <amit.sangwan@box8.in>
2018-09-30 12:55:15 +0530

    sTARTED WORKING ON PROJECT

---
+++ CheckList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckList
+//
+//  Created by Amit Kumar on 30/09/18.
+//  Copyright © 2018 Box8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
